54956ec7790771331bcf20f928d27c00e6da1f1d
matias <andresmatias@gmail.com>
2025-07-03 13:15:14 -0400

    pokemon

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import react from "react";
+import PokemonFetcher from "./pokemonFetcher";
+
+function App() {
+  return(
+    <>
+    <h1>¡conoce a tus pokemon!</h1>
+    <PokemonFetcher/>
+    </>
+  );
+}
+
+export default App
